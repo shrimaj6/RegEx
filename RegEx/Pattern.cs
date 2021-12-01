269ed729f9ae8b18b1e61d9940b706f0ea57c1f9
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-01 11:52:29 +0530

    Add project files.

---
+++ RegEx/Pattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    public class Pattern
+    {
+        public static void FirstName(string name)
+        {
+            string FirstName = "^[A-Z][a-zA-Z]{1,10}$"
+;
+
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+
+        }
+    }
+}
---
+++ RegEx/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program \n");
+            Console.WriteLine("Write your name using 1st character should be Capital ");
+            Console.Write("\n Enter First Name : ");
+            string name = Console.ReadLine();
+            Pattern.FirstName(name);
+        }
+    }
+}
